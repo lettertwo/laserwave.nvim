b05ca4b239bcadfe0a6db6e8e77069a452a0583b
lettertwo <lettertwo@gmail.com>
2022-04-24 21:35:59 -0400

    Add vim compat

--- colors/laserwave.lua
+++
@@ -1,6 +0,0 @@
-package.loaded["lush_theme.laserwave"] = nil
-package.loaded["laserwave"] = nil
-package.loaded["laserwave.base"] = nil
-package.loaded["laserwave.config"] = nil
-
-require("lush")(require("laserwave").setup())
--- lua/laserwave/init.lua
+++ lua/laserwave/init.lua
@@ -1,18 +1,19 @@
-local lush = require("lush")
-local base = require("laserwave.base")
-local defaults = require("laserwave.config")
-
 ---@param options Config
-local function setup(options)
+local function config(options)
+  package.loaded["laserwave.base"] = nil
+  package.loaded["laserwave.config"] = nil
+
+  local base = require("laserwave.base")
+  local defaults = require("laserwave.config")
+
   vim.o.background = "dark"
   vim.g.colors_name = "laserwave"
 
-  -- if options then
-  --   if options.style == "light" then
-  --     vim.o.background = "light"
-  --   end
-  -- end
   return defaults.apply(base, options)
 end
 
-return { setup = setup }
+local function apply()
+  require("lush")(config(), { force_clean = false })
+end
+
+return { config = config, apply = apply }
