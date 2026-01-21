5eca270bf54d5e9a3fc005d2610cd3f6dcfbdcf4
Eric Eldredge <lettertwo@gmail.com>
2026-01-21 16:07:51 -0500

    fix(spec)!: remove majority of semantic highlights
    These are largely redundant with treesitter thanks to neovim's sane
    defaults for semantic tokens.

--- lua/laserwave/spec/semantic_highlights.lua
+++ lua/laserwave/spec/semantic_highlights.lua
@@ -1,53 +1,20 @@
-local palette = require("laserwave.palette")
-
+-- Semantic highlights for LSP.
+-- Note that the majority of common semantic tokens are linked to
+-- Tree-sitter highlight groups by default in Neovim, so only overrides
+-- and additions are necessary here.
+--
+-- NOTE: Treesitter must be enabled for LSP semantic highlighting to work as intended.
+--
+-- See: :h lsp-semantic-highlight
+--
 ---@type laserwave.Spec
 --stylua: ignore
 local semantics = {
-  ["@lsp.type.boolean"]                      = "@boolean",
-  ["@lsp.type.builtinType"]                  = "@type.builtin",
-  ["@lsp.type.comment"]                      = "@comment",
-  ["@lsp.type.decorator"]                    = "@attribute",
-  ["@lsp.type.deriveHelper"]                 = "@attribute",
-  ["@lsp.type.enum"]                         = "@type",
-  ["@lsp.type.enumMember"]                   = "@constant",
-  ["@lsp.type.escapeSequence"]               = "@string.escape",
-  ["@lsp.type.formatSpecifier"]              = "@markup.list",
-  ["@lsp.type.generic"]                      = "@variable",
-  ["@lsp.type.interface"]                    = "@lsp.type",
-  ["@lsp.type.keyword"]                      = "@keyword",
-  ["@lsp.type.keyword.lua"]                  = "@lsp.type.keyword",
-  ["@lsp.type.class"]                        = "@none",
-  ["@lsp.type.class.lua"]                    = "@lsp.type.class",
-  ["@lsp.type.lifetime"]                     = "@keyword.storage",
-  ["@lsp.type.namespace"]                    = "@module",
-  ["@lsp.type.namespace.python"]             = "@variable",
-  ["@lsp.type.number"]                       = "@number",
-  ["@lsp.type.operator"]                     = "@operator",
-  ["@lsp.type.parameter"]                    = "@variable.parameter",
-  ["@lsp.type.property"]                     = "@property",
-  ["@lsp.type.selfKeyword"]                  = "@variable.builtin",
-  ["@lsp.type.selfTypeKeyword"]              = "@variable.builtin",
-  ["@lsp.type.string"]                       = "@string",
-  ["@lsp.type.typeAlias"]                    = "@type.definition",
-  ["@lsp.type.unresolvedReference"]          = { undercurl = true, sp = palette.ERROR },
-  ["@lsp.type.variable"]                     = "@variable",
-  ["@lsp.typemod.class.defaultLibrary"]      = "@type.builtin",
-  ["@lsp.typemod.enum.defaultLibrary"]       = "@type.builtin",
-  ["@lsp.typemod.enumMember.defaultLibrary"] = "@constant.builtin",
-  ["@lsp.typemod.function.defaultLibrary"]   = "@function.builtin",
-  ["@lsp.typemod.keyword.async"]             = "@keyword.coroutine",
-  ["@lsp.typemod.keyword.injected"]          = "@keyword",
-  ["@lsp.typemod.macro.defaultLibrary"]      = "@function.builtin",
-  ["@lsp.typemod.method.defaultLibrary"]     = "@function.builtin",
-  ["@lsp.typemod.operator.injected"]         = "@operator",
-  ["@lsp.typemod.string.injected"]           = "@string",
-  ["@lsp.typemod.struct.defaultLibrary"]     = "@type.builtin",
-  ["@lsp.typemod.type.defaultLibrary"]       = "@type.builtin",
-  ["@lsp.typemod.typeAlias.defaultLibrary"]  = "@type.builtin",
-  ["@lsp.typemod.variable.callable"]         = "@function",
-  ["@lsp.typemod.variable.defaultLibrary"]   = "@variable.builtin",
-  ["@lsp.typemod.variable.injected"]         = "@variable",
-  ["@lsp.typemod.variable.static"]           = "@constant",
+	["@lsp.type.enumMember"]                 = "@constant",
+	["@lsp.type.variable"]                   = {}, -- Use treesitter's variable highlighting
+	["@lsp.typemod.function.defaultLibrary"] = "@function.builtin",
+	["@lsp.typemod.function.builtin"]        = "@function.builtin",
+  ["@lsp.typemod.variable.static"]         = "@constant",
 }
 
 return semantics
